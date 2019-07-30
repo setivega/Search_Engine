6fe712d5e6b3e1f42a24e2024e7b20c1b407d0af
shiloh <shilohwilliams19@gmail.com>
2019-07-30 15:20:15 -0500

    functionality

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import random
+from tkinter import*
+import tkinter as tk
+from random import *
+from tkinter import messagebox
+from os import *
+from PIL import Image
+window = Tk()
+window.title("PHROG")
+window.geometry("1440x900")
+window.configure(background = 'grey')
+
+path ="SLAB.png"
+img = PhotoImage(Image.open(path))
+panel = Label(window, image = img)
+window.mainloop()
+# theLabel.mainloop()
+# def setup():
+#     global picList,img,img2,img3,index,user,onScreen,keyboard,j
+#     size(1440,900)
+#     img = loadImage("SLAB.jpg")
+#     img2 = loadImage("GGB.jpg")
+#     img3 = loadImage("PHROG.jpg")
+#     user = " "
+#     picList = [img, img2, img3]
+#     index = int(randint(0, len(picList)-1))
+#     background(picList[index])

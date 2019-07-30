f169fc897421fc6527e0f615f39996a9e7c6893a
setivega <setimvega@gmail.com>
2019-07-30 16:19:07 -0400

    Added tkinter

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import tkinter as tk
 from tkinter import *
+from PIL import ImageTk, Image
+
+#This creates the main window of an application
+window = tk.Tk()
+window.title("Phrog")
+window.geometry("1440x900")
+window.configure(background='grey')
+
+path = "Super.png"
+
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+img = ImageTk.PhotoImage(Image.open(path))
+
+#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+panel = tk.Label(window, image = img)
+
+#The Pack geometry manager packs widgets in rows or columns.
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+
+#Start the GUI
+window.mainloop()

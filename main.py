022d55662818e3880165db4cc951423fa2be7ebe
shiloh <shilohwilliams19@gmail.com>
2019-08-01 09:33:45 -0500

    test2

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import tkinter as tk
 from tkinter import *
 from PIL import ImageTk, Image
 from random import *
+import main
 
 #This creates the main window of an application
-window = tk.Tk()
-window.title("Phrog")
-window.geometry("1440x900")
-window.configure(background='grey')
+# window = tk.Tk()
+# window.title("Phrog")
+# window.geometry("1440x900")
+# window.configure(background='grey')
 
-path = "Images/SLAB.png"
-path2 = "Images/GGB.png"
-path3 = "Images/PHROG.png"
-path4 = "Images/space.png"
-picList = [path,path2,path3,path4]
-#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-index = (randint(0, len(picList)-1))
-img = ImageTk.PhotoImage(Image.open(picList[index]))
+def backgroundWindow():
+    path = "Images/SLAB.png"
+    path2 = "Images/GGB.png"
+    path3 = "Images/PHROG.png"
+    path4 = "Images/space.png"
+    picList = [path,path2,path3,path4]
+    #Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+    index = (randint(0, len(picList)-1))
+    img = ImageTk.PhotoImage(Image.open(picList[index]))
 
-#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-panel = tk.Label(window, image = img)
-
-#The Pack geometry manager packs widgets in rows or columns.
-panel.pack(side = "bottom", fill = "both", expand = "yes")
-
-#Start the GUI
-window.mainloop()
+    # # #The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+    # panel = tk.Label(window, image = img)
+    # #
+    # # #The Pack geometry manager packs widgets in rows or columns.
+    # panel.pack(side = "bottom", fill = "both", expand = "yes")
+    # #Start the GUI
+    # window.mainloop()
--- news_search.py
+++ news_search.py
@@ -3,8 +3,12 @@ import urllib.parse
 import tkinter as tk
 from tkinter import *
 from PIL import ImageTk, Image
+from main import backgroundWindow
 
-query = 'how to put music in'
+# puzzle = input(" ")
+# entry_1 = Entry("")
+# content = entry_1.get()
+query = 'bacon'
 apiKey = '5ed40e8e488f4b26a289403dd6c181ef'
 mainApi = 'https://newsapi.org/v2/everything?'
 
@@ -31,22 +35,23 @@ for article in articles:
 print(news)
 
 def createArticles():
-    global news
+    global news, content,entry_1
     yCoord = 200
     for article in news:
         titleLabel = Label(window, text = article['title'])
-        titleLabel.place(x = 20,y = yCoord)
+        titleLabel.place(x = 200,y = yCoord)
         nameLabel = Label(window, text = article['name'])
-        nameLabel.place(x = 20,y = yCoord+50)
+        nameLabel.place(x = 200,y = yCoord+50)
         descriptionLabel = Label(window, text = article['description'])
-        descriptionLabel.place(x = 20,y = yCoord+100)
+        descriptionLabel.place(x = 200,y = yCoord+100)
         yCoord+= 200
 
 #This creates the main window of an application
 window = Tk()
+pic = backgroundWindow()
 window.title("Phrog")
 window.geometry("1440x900")
-window.configure(background='white')
+window.configure(background= pic)
 t = Text()
 t.config(wrap = WORD)
 
@@ -54,10 +59,23 @@ logoPath = "Images/logo.png"
 
 logo = ImageTk.PhotoImage(Image.open(logoPath))
 
-panel = Label(window, image = logo)
-panel.place(x=20,y=30)
-# panel.pack(side="top", fill='both', expand=True, padx=4, pady=4)
 
+Label_1 = Label(window, text="Search Bar ")
+entry_1 = Entry(window)
+Label_1.place(x=720, y=150)
+entry_1.place(x=720, y=150)
+
+# panel = Label(window, image = logo)
+# panel.place(x=20,y=30)
+# # panel.pack(side="top", fill='both', expand=True, padx=4, pady=4)
+# sBar = tk.Label(window, text = "SEARCH")
+# sBar.grid(row=500, column=500)
+# sBar.pack()
+# button = tk.Button(window, text='click', command=Clicked)
+# button.pack()
+# content = StringVar()
+# entry = tk.Entry(window,textVariable=content)
+# entry.pack()
 
 searchBar = Entry(window)
 
